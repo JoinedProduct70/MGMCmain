5b5f3b14804224870f84bfdbdf7d1c4d5e667f19
JoinedProduct70 <ethanburnett123@outlook.com>
2015-04-09 19:30:30 -0500

    Create info_command.java

---
+++ src/config/commands/info_command.java
@@ -0,0 +1,10 @@
+
+public class info_command.java {
+
+  player.sendMessage(ChatColor.RED + "Welcome to MCSG, the survivalgames/minigames based server!");
+  player.sendMessage(ChatColor.RED + "The owner is EthanPlayz, He also SELF coded this plugin.");
+  player.sendMessage(ChatColor.RED + "To /adminhelp, to global chat a help message! Good luck!");
+  // simple command :/
+  return true;
+
+ 
